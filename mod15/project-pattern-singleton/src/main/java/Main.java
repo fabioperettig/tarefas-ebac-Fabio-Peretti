c34dfe90c9738ca197609d8ffb3a36bf4e43e0d5
Fabio Peretti Guimarães <fabioperetti_pp@outlook.com>
2026-02-20 17:55:05 -0300

    jakarta update

--- mod15/project-pattern-singleton/src/main/java/Main.java
+++ mod15/project-pattern-singleton/src/main/java/Main.java
@@ -1,4 +1,7 @@
-import br.com.fabioperettig.designpatterns.singleton.SystemHealthMonitor;
+import br.com.fabioperettig.designpatterns.jakarta.HealthController;
+
+import jakarta.enterprise.inject.se.SeContainer;
+import jakarta.enterprise.inject.se.SeContainerInitializer;
 
 public class Main {
 
@@ -6,10 +9,24 @@ public class Main {
         String service = "default value";
         String status = "offline";
 
+        System.out.println("––– INIT –––");
+
+        try (SeContainer container = SeContainerInitializer.newInstance().initialize()){
+            HealthController c1 = container.select(HealthController.class).get();
+            HealthController c2 = container.select(HealthController.class).get();
+
+            c1.executeCheckup();
+            c2.executeCheckup();
+
+        }
+
+        System.out.println("––– END –––");
+
+        /*
         SystemHealthMonitor monitor = SystemHealthMonitor.getInstance();
         monitor.updateStatus(service,status);
 
-        monitor.printReport();
+        monitor.printReport();*/
     }
 
 }
