c3f1c4d13da8d9872f268de3f1a6c32b1053adae
Fabio Peretti Guimarães <fabioperetti_pp@outlook.com>
2026-03-23 09:07:56 -0300

    study upload

---
+++ mod17/generic-study-class/GenericClass.java
@@ -0,0 +1,31 @@
+/**
+ * With this param T tagged in the Class, it's possible to implement
+ * attributes and constructors without specify what kind of attribute/param
+ * you want it. For this reason, you can instance and obj 'gen1 = new GenericClass(string)'
+ * and, with setter, is even possible declare 't1.setData(123)' turning String param into an Integer.
+ *
+ * But BEWARE: using this kind of instance, Java will alert you
+ * "Raw use of parameterized class 'GenericClass'", suggesting you declare
+ * the exactly Type od you Object.
+ *
+ * GenericClass t1 = new GenericClass<>(1); [DANGER: RAW TYPE]
+ * GenericClass<Integer> t1 = new GenericClass<>(1); [OK]
+ *
+ * @param <T>
+ */
+public class GenericClass <T>{
+
+    private T data;
+
+    public GenericClass(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
--- mod17/generic-study-class/Main.java
+++ mod17/generic-study-class/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        GenericClass t1 = new GenericClass<>(1);
+        System.out.println(t1.getData());
+
+
     }
 
 }
