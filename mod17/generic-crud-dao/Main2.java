f0668c7f11a820e08806f073ca689d4fc48ca92f
Fabio Peretti Guimarães <fabioperetti_pp@outlook.com>
2026-03-29 02:38:51 -0300

    CRUD with Generic implementation

--- mod17/generic-crud-dao/Main2.java
+++ mod17/generic-crud-dao/Main2.java
@@ -34,14 +34,22 @@ public class Main {
                 String data = JOptionPane.showInputDialog(null,
                         "Enter the user's information separated by commas, as in the example: Name, ID, Country.",
                         "Register", JOptionPane.INFORMATION_MESSAGE);
-
                 register(data);
 
             } else if (isRead(option)) {
                 String data = JOptionPane.showInputDialog(null,
                 "Enter the code", "Search", JOptionPane.INFORMATION_MESSAGE);
-
                 check(data);
+
+            } else if (isUpdate(option)) {
+                String data = JOptionPane.showInputDialog(null,
+                        "Enter the code", "Search", JOptionPane.INFORMATION_MESSAGE);
+                update(data);
+
+            } else if (isDelete(option)) {
+                String data = JOptionPane.showInputDialog(null,
+                        "Enter the code", "Search", JOptionPane.INFORMATION_MESSAGE);
+                delete(data);
             }
 
             option = JOptionPane.showInputDialog(null,
@@ -51,6 +59,12 @@ public class Main {
         }
 
     }
+
+    private static boolean isValidOption(String option){
+        return "1".equals(option) || "2".equals(option) || "3".equals(option) ||
+                "4".equals(option)|| "5".equals(option);
+    }
+
 
     /**
      * Checks if the provided option corresponds to the 'create' action.
@@ -95,9 +109,43 @@ public class Main {
         }
     }
 
-    private static boolean isValidOption(String option){
-        return "1".equals(option) || "2".equals(option) || "3".equals(option) ||
-                "4".equals(option)|| "5".equals(option);
+    private static boolean isUpdate(String option){
+        return "3".equals(option);
+    }
+
+    private static void update (String option){
+        String data = JOptionPane.showInputDialog(null,
+                "Enter the user's information separated by commas, as in the example: Name, ID, Country.",
+                "Update", JOptionPane.INFORMATION_MESSAGE);
+
+        String[] arrayData = data.split(",");
+        Client client = new Client(arrayData[0], Long.valueOf(arrayData[1]), arrayData[2]);
+
+        iClientDAO.update(client);
+        JOptionPane.showMessageDialog(null, "Client updated with success!",
+                "Success", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private static boolean isDelete(String option){
+        return "4".equals(option);
+    }
+
+    private static void delete(String data){
+        Client client = iClientDAO.read(Long.parseLong(data));
+        if (client != null) {
+
+        }
+    }
+
+    private static void check (String data){
+        Client client = iClientDAO.read(Long.parseLong(data));
+        if (client != null) {
+            JOptionPane.showMessageDialog(null, "Client found: "
+                    + client.toString(),"Success", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, "Client not found.",
+                    "Failure", JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 
     private static boolean isExitOption(String option) {
