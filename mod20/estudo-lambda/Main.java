78a9585fe955946a87f192687e66ed7cadbd62b2
Fabio Peretti Guimarães <fabioperetti_pp@outlook.com>
2026-04-14 22:31:28 -0300

    study update

--- mod20/estudo-lambda/Main.java
+++ mod20/estudo-lambda/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args){
 		System.out.println("Hello World");
+
+        List<String> list = List.of("Zeke", "Julie", "Zombie", "Werewolf");
+
+        list.forEach((String lprint) -> {
+                System.out.printf("\nName on List: %s", lprint);
+            }
+        );
 	}
 }
