b8b9a9feb692346b3b80d33e1f15db4c3954ed0b
Fabio Peretti Guimarães <fabioperetti_pp@outlook.com>
2026-09-22 11:46:48 -0300

    Testes Cursos finalizados

--- mod32/tarefa-mod-32/src/test/java/CursoDaoTest.java
+++ mod32/tarefa-mod-32/src/test/java/CursoDaoTest.java
@@ -4,6 +4,9 @@ import com.fabioperettig.domain.Curso;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CursoDaoTest {
 
     private ICursoDao cursoDao;
@@ -64,7 +67,7 @@ public class CursoDaoTest {
     public void removerCurso() {
         Curso curso = new Curso();
         curso.setCategoria("backend");
-        curso.setCodigo("CRS004");
+        curso.setCodigo("CRS000");
         curso.setNome("JDBC");
 
         cursoDao.create(curso);
@@ -75,6 +78,61 @@ public class CursoDaoTest {
 
         Curso cursoDEL = cursoDao.readById(curso.getId());
         Assertions.assertNull(cursoDEL);
+    }
+
+    @Test
+    public void listarCursos() {
+
+        List<Curso> listaCursosEsperados = new ArrayList<>();
+        criarCursosLista(listaCursosEsperados);
+        Assertions.assertNotNull(listaCursosEsperados);
+
+        List<Curso> listaResult = cursoDao.findAll();
+        Assertions.assertNotNull(listaResult);
+
+        for (Curso esperado : listaCursosEsperados) {
+            Assertions.assertTrue(
+                    listaResult.stream().anyMatch(
+                            encontrado -> esperado.getId().equals(encontrado.getId())
+                    )
+            );
+        }
+
+    }
+
+    public List<Curso> criarCursosLista(List<Curso> listaCursos) {
+
+        Curso curso1 = new Curso();
+        curso1.setCategoria("frontend");
+        curso1.setCodigo("CRS004");
+        curso1.setNome("HTML5");
+
+        Curso curso2 = new Curso();
+        curso2.setCategoria("backend");
+        curso2.setCodigo("CRS005");
+        curso2.setNome("Spring Boot");
+
+        Curso curso3 = new Curso();
+        curso3.setCategoria("database");
+        curso3.setCodigo("CRS006");
+        curso3.setNome("PSQL");
+
+        Curso curso4 = new Curso();
+        curso4.setCategoria("backend");
+        curso4.setCodigo("CRS007");
+        curso4.setNome("Java - Implementando com Generics");
+
+        cursoDao.create(curso1);
+        cursoDao.create(curso2);
+        cursoDao.create(curso3);
+        cursoDao.create(curso4);
+
+        listaCursos.add(curso1);
+        listaCursos.add(curso2);
+        listaCursos.add(curso3);
+        listaCursos.add(curso4);
+
+        return listaCursos;
     }
 
 }
